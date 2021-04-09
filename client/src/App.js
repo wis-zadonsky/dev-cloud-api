f43fc9baf5766f99c32df32ae4e9ea61ae84ae30
k1k8r <andreyzadonsky02@mail.ru>
2021-04-09 21:06:47 +0300

    User token save

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Login } from './components/auth/Login';
@@ -9,8 +9,19 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if(localStorage.token) {
+	setAuthToken(localStorage.token)
+}
 
 const App = () => {
+
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Router>
