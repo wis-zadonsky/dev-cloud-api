4d8ce27bd4a529b68bffe1adc5b8f0af7dbf1b71
Задонский Андрей Владимирович <a.zadonsky@kmd24.com>
2022-06-22 23:53:44 +0300

    add versel config

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
